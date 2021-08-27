480843b441e7932124be2191888f7d3640292792
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-08-27 00:02:10 -0600

    correcciones, adiciones

--- src/Factory/DC.java
+++ src/Factory/DC.java
@@ -2,11 +2,13 @@ package Factory;
 
 import Heroe.Heroe;
 import Villano.Villano;
+import Heroe.HeroeDC;
+import Villano.VillanoDC;
 
 public class DC implements AbstractFactory{
     @Override
-    public Heroe getHeroe() { return null; }
+    public Heroe getHeroe() { return new HeroeDC("Batman", "x", 5,5); }
 
     @Override
-    public Villano getVillano() { return null; }
+    public Villano getVillano() { return new VillanoDC("x","x", 5, 5); }
 }
--- src/Heroe/HeroeDC.java
+++ src/Heroe/HeroeDC.java
@@ -3,7 +3,7 @@ package Heroe;
 public class HeroeDC extends Heroe{
 
 
-    public HeroeDC(String nombre, String poder, String vidas, String energia) {
+    public HeroeDC(String nombre, String poder, int vidas, int energia) {
         super(nombre, poder, vidas, energia);
     }
 }
